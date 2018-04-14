ceaa921a6b18d965436d31a422d7454094e09ea7
C-E-P <c.elena.palmer@outlook.com>
2018-04-14 18:47:10 -0500

    tweak naming conventions

--- flask_tus.py
+++ flask_tus.py
@@ -100,7 +100,7 @@ class tus_manager(object):
                 return response
 
             response.status_code = 201
-            response.headers['Location'] = '{}/{}/{}'.format(request.url_root, self.upload_url, resource_id)
+            response.headers['Location'] = '{}/{}/{}'.format(request.url_root, self.upload_url, resource_id + '-'+ self.upload_info['upload_filename'])
             response.headers['Tus-Temp-Filename'] = resource_id
             response.autocorrect_location_header = False
 
